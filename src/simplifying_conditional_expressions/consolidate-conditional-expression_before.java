859112961fc155e3aa84121dd5fb98d1407bb3a3
indhira15 <indhira.ramirez@utec.edu.pe>
2019-06-03 10:00:15 -0500

    part5

--- src/simplifying_conditional_expressions/consolidate-conditional-expression_before.java
+++ src/simplifying_conditional_expressions/consolidate-conditional-expression_before.java
@@ -1,11 +1,5 @@
 double disabilityAmount() {
-  if (seniority < 2) {
-    return 0;
-  }
-  if (monthsDisabled > 12) {
-    return 0;
-  }
-  if (isPartTime) {
+  if (seniority < 2 || monthsDisabled > 12) || isPartTime){
     return 0;
   }
   // Compute the disability amount.
